2e4369435236d52d59f5987efdefdee30c1769cf
Pavan Mistry <pavan32100@gmail.com>
2024-05-15 17:33:14 -0400

    Week 2 updates

---
+++ week2/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+
+<body>
+    <?php
+    
+    $fname = "Pavan";
+    $lname = "Mistry";
+    echo '<h1>Welcome to HTTP 5225 '. $fname . ' ' . $lname . '</h1>
+    <p>We will learn PHP in this class, (maybe?)</p>
+    <p>Things we will learn in this class: </p>
+    <ul>
+    <li>PHP</li>
+    <li>MYSQL</li>
+    <li>LARAVEL</li>
+    </ul>';
+
+    ?>
+    <?php echo '<img src="https://placehold.co/600x400" alt="Placeholder image">'; ?>
+
+    <h1>Welcome to HTTP 5225</h1>
+    <p>We will learn PHP in this class, (maybe?)</p>
+    <p>Things we will learn in this class: </p>
+    <ul>
+        <li>PHP</li>
+        <li>MYSQL</li>
+        <li>LARAVEL</li>
+    </ul>
+    <img src="https://placehold.co/600x400" alt="Placeholder image">
+
+</body>
+
+</html>
