7b485ba59cbb837b822678e70aa1689bfc994254
PAVAN\pavan <pavan32100@gmail.com>
2024-08-01 22:44:29 -0400

    LMS class changes

--- lms/database/seeders/DatabaseSeeder.php
+++ lms/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Student;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Pavan Mistry',
+            'email' => 'pavan32100@gmail.com',
         ]);
+
+        Student::factory(20)->create();
     }
 }
